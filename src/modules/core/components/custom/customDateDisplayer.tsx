a0003615175e977dddc9f3813fbd45b4f5f0102d
presori <presori@boom.al>
2026-07-12 00:39:25 +0200

    Missing language kyes fixes

--- src/modules/core/components/custom/customDateDisplayer.tsx
+++ src/modules/core/components/custom/customDateDisplayer.tsx
@@ -86,7 +86,7 @@ function CustomDateDisplayer({
             }
         ));
 
-    }, [timeZone, date, showOnlyWeekDay, showOnlyYesterday]);
+    }, [timeZone, date, showOnlyWeekDay, showOnlyYesterday, resolveLanguageKey]);
 
     return (
         <TooltipDisplayer tooltip={fullDate}>
